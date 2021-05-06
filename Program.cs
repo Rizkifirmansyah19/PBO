905b440f55eaf61d15bd1544bb19a82667b7abc5
Rizkifirmansyah19 <83721212+Rizkifirmansyah19@users.noreply.github.com>
2021-05-06 14:16:37 +0700

    DESIMAL KE HEXADESIMAL

--- Program.cs
+++ Program.cs
@@ -1,38 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ConsoleApplication1
+public class Exercise30
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("----------------------------------------------------------");
-            Console.WriteLine("Tugas Program Konversi Dari Bilangan Desimal Ke Biner  Visual C#");
-            Console.WriteLine("----------------------------------------------------------");
-            Console.WriteLine("Nama        : Rizki Firmansyah");
-            Console.WriteLine("NPM         : 1101201062");
-            Console.WriteLine("Kelas       : Malam");
-            Console.WriteLine("Dosen       : Asep Mabrur Aid,S.T,M.Kom");
-            Console.WriteLine("Prodi       : Teknik Informatika (Malam)");
-            Console.WriteLine("Semester    : 2 (Genap)");
-            Console.WriteLine("Mata Kuliah : Pemrograman Berorientasi Objek");
-            Console.WriteLine("----------------------------------------------------------");
-            Console.Write("Input Angka Desimal Yang akan di Konversi Yaitu 4 :  ");
 
 
-            int bilangan, sisa;
-            string hasil = "";
-            bilangan = Convert.ToInt32(Console.ReadLine());
-            while (bilangan > 0)
-            {
-                sisa = bilangan % 4;
-                bilangan /= 4;
-                hasil = sisa.ToString() + hasil;
-            }
-            Console.WriteLine("Hasil Konversi Ke Biner = 10 : {0}", hasil);
-        }
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("Tugas Program Bilangan Desimal Ke Hexadesimal Visual C#");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("Nama      : Rizki Firmansyah");
+        Console.WriteLine("Npm       : 1101201062");
+        Console.WriteLine("Kelas     : Malam");
+        Console.WriteLine("Dosen     : Asep Mabrur Aid,S.T,M.Kom");
+        Console.WriteLine("Prodi     : Teknik Informatika");
+        Console.WriteLine("MK        : Pemgraman Berorientasi Objek");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        string hexval = "4B0";
+        Console.WriteLine("Hexadecimal number: " + hexval);
+        int decValue = int.Parse(hexval, System.Globalization.NumberStyles.HexNumber);
+        Console.WriteLine("Convert to-");
+        Console.WriteLine("Decimal number: " + decValue);
     }
 }
+
